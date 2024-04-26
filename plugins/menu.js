532ca1bafbb6fb92c9da2755a32e88777773d16d
alfred <100863418+RadouaneElarfaoui@users.noreply.github.com>
2024-04-26 02:13:44 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -28,7 +28,7 @@ const defaultMenu = {
 ⏰ *Uptime:* %muptime  
 ┗━━━━━━━━━━⬣
 %readmore
-  ≡ *B O B I Z A | M E N U*
+  ≡ *ELARFAOUI BOT| M E N U*
 `.trimStart(),
   header: '┏━━⊜ *_%category_* ',
   body: '┃⋄ %cmd %isdiamond %isPremium',
@@ -136,10 +136,10 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 text: text,
 contextInfo: {
 externalAdReply: {
-title: 'BOBIZA BOT ♥',
-body: "أول بوت واتساب في العالم العربي 💖",
+title: 'ELARFAOUI BOT',
+body: "Welcome to my world ",
 thumbnailUrl: 'https://telegra.ph/file/2829c7653514416d207e2.jpg',
-sourceUrl: 'https://instagram.com/noureddine_ouafy',
+sourceUrl: 'https://www.instagram.com/a.r.f.radouane?igsh=MWpneXB1dDIzcmJ4ag==',
 mediaType: 1,
 renderLargerThumbnail: true
 }}}, { quoted: m})
